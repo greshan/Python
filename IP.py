2e37aeabc5bb8e65250d02a1c5a5c33115d2acfe
greshan <42962555+greshan@users.noreply.github.com>
2018-10-09 20:26:42 +0530

    Update IP.py

--- IP.py
+++ IP.py
@@ -1,10 +1,11 @@
 import sys
 
 args=sys.argv[1]
-
+count = 0;
 with open("IP.txt") as f:
 	for line in f:
 		if args in line:
-			print("IP present: ",args)
-
-sys.exit() 
+			count = count+1;
+print("IP present: ",args)
+print("No of times the ip repeats: ",count)
+sys.exit()
